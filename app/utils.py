9aa0888764966837740b2f42b273bc3a62a6f614
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-07-04 16:52:49 +0700

    BE done

---
+++ app/utils.py
@@ -0,0 +1,29 @@
+from os import path
+import pandas as pd
+from django.conf import settings
+
+
+def clean_month(month):
+    month_list = ['januari', 'febuari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober',
+                  'november', 'desember']
+
+    return month_list.index(month) + 1
+
+
+def clean_quality(quality):
+    a = 1
+    if quality.lower() == 'premium':
+        a = 3
+    elif quality.lower() == 'medium':
+        a = 2
+
+    return a
+
+
+def data_frame():
+    df = pd.read_csv(path.join(settings.BASE_DIR, 'static', 'data_beras.csv'))
+    df['bulan'] = df['bulan'].apply(clean_month)
+    df['kualitas'] = df['kualitas'].apply(clean_quality)
+
+    return df.iloc[:, :-1], df.iloc[:, -1]
+
--- app/views.py
+++ app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic import *
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
 from sklearn.linear_model import LinearRegression
+from .utils import *
+import numpy as np
 
 
 def get_template(temp):
@@ -16,11 +17,31 @@ class LandingPage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         model = LinearRegression()
-        month_list = ['januari', 'febuari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'septermber', 'oktober', 'november', 'desember']
+        month_list = ['januari', 'febuari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober',
+                      'november', 'desember']
+
+        X, y = data_frame()
+        model.fit(X, y)
+        coef = model.coef_
+        rank = model.rank_
+        intercept = model.intercept_
+        score = model.score(X, y)
+
+        if self.request.GET.get('year') is not None:
+            year = int(self.request.GET.get('year')) if self.request.GET.get('year') is not None else None
+            month = self.request.GET.get('month')
+            month = clean_month(month)
+
+            qlt = self.request.GET.get('qlt')
+            qlt = clean_quality(qlt)
+
+            pred = np.array([[year, qlt, month]])
+            predict = model.predict(pred)
+            context['predict'] = predict
 
         context['months'] = month_list
         return context
 
-    @method_decorator(login_required(login_url=settings.LOGIN_URL))
-    def dispatch(self, request, *args, **kwargs):
-        return super().dispatch(request, *args, **kwargs)
+    # @method_decorator(login_required(login_url=settings.LOGIN_URL))
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super().dispatch(request, *args, **kwargs)
