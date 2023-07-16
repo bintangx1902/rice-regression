7c8309f428a253aa37a4303798808e88b5cf8d89
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-07-16 23:03:07 +0700

    set to all host

--- rice_regression/settings.py
+++ rice_regression/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ogw14wxomrq6lb6*zfzq&ghfxwqxs-f&8zj9(k6+h9djff!dj#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
