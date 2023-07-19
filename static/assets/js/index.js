c3b60c99402a8d43da538789533783959cee58d5
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2023-07-20 00:36:17 +0700

    new overwrite

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let dataBeras = []
+let chart
 
 document.querySelector("#year-chooser-input").addEventListener("change", (element) => {
 	let choosenYear = element.target.value
@@ -14,25 +14,120 @@ document.querySelector("#year-chooser-input").addEventListener("change", (elemen
 	$("#medium").append("<th>Medium</th>")
 	$("#luar").append("<th>Luar Kualitas</th>")
 
-	beras.forEach((beras) => {
-		if (beras.tahun == choosenYear) {
-			if (beras.kualitas == "Premium") {
-				$("#premium").append(`
-					<td>${beras.harga}</td>
-				`)
-			}
+	beras[choosenYear].premium.forEach((hargaBeras) => {
+		$("#premium").append(`
+			<td>${hargaBeras}</td>
+		`)
+	})
 
-			if (beras.kualitas == "Medium") {
-				$("#medium").append(`
-					<td>${beras.harga}</td>
-				`)
-			}
+	beras[choosenYear].medium.forEach((hargaBeras) => {
+		$("#medium").append(`
+			<td>${hargaBeras}</td>
+		`)
+	})
 
-			if (beras.kualitas == "Luar Kualitas") {
-				$("#luar").append(`
-					<td>${beras.harga}</td>
-				`)
-			}
-		}
+	beras[choosenYear].luarKualitas.forEach((hargaBeras) => {
+		$("#luar").append(`
+			<td>${hargaBeras}</td>
+		`)
 	})
 })
+
+document.querySelector("#diagram-year-chooser-input").addEventListener("change", (element) => {
+	chart?.destroy()
+	let choosenYear = element.target.value
+	let ctx = document.getElementById("ricePriceChart").getContext("2d")
+
+	chart = chart = new Chart(ctx, {
+		type: "bar",
+		data: {
+			labels: ["January", "February", "March", "June", "July", "August", "September", "October", "November", "December"],
+			datasets: [
+				{
+					label: "Premium",
+					data: beras[choosenYear].premium,
+					backgroundColor: "rgba(255, 99, 132, 0.5)",
+					borderColor: "rgba(255, 99, 132, 1)",
+					borderWidth: 1,
+				},
+				{
+					label: "Medium",
+					data: beras[choosenYear].medium,
+					backgroundColor: "rgba(54, 162, 235, 0.5)",
+					borderColor: "rgba(54, 162, 235, 1)",
+					borderWidth: 1,
+				},
+				{
+					label: "Luar kualitas",
+					data: beras[choosenYear].luarKualitas,
+					backgroundColor: "rgba(75, 192, 192, 0.5)",
+					borderColor: "rgba(75, 192, 192, 1)",
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+			responsive: true,
+			title: {
+				display: true,
+				text: "Rise in Rice Prices (2023)",
+			},
+			scales: {
+				y: {
+					beginAtZero: true,
+					title: {
+						display: true,
+						text: "Price (in Rupiah)",
+					},
+				},
+			},
+		},
+	})
+})
+
+// var ctx = document.getElementById("ricePriceChart").getContext("2d")
+// var chart = new Chart(ctx, {
+// 	type: "bar",
+// 	data: {
+// 		labels: ["June", "July", "August", "September", "October", "November", "December"],
+// 		datasets: [
+// 			{
+// 				label: "Premium",
+// 				data: [15000, 16000, 16500, 17000, 17500, 18000, 19000],
+// 				backgroundColor: "rgba(255, 99, 132, 0.5)",
+// 				borderColor: "rgba(255, 99, 132, 1)",
+// 				borderWidth: 1,
+// 			},
+// 			{
+// 				label: "Medium",
+// 				data: [13000, 14000, 14500, 15000, 15500, 16000, 16500],
+// 				backgroundColor: "rgba(54, 162, 235, 0.5)",
+// 				borderColor: "rgba(54, 162, 235, 1)",
+// 				borderWidth: 1,
+// 			},
+// 			{
+// 				label: "Luar kualitas",
+// 				data: [10000, 10500, 11000, 11500, 12000, 12500, 13000],
+// 				backgroundColor: "rgba(75, 192, 192, 0.5)",
+// 				borderColor: "rgba(75, 192, 192, 1)",
+// 				borderWidth: 1,
+// 			},
+// 		],
+// 	},
+// 	options: {
+// 		responsive: true,
+// 		title: {
+// 			display: true,
+// 			text: "Rise in Rice Prices (2023)",
+// 		},
+// 		scales: {
+// 			y: {
+// 				beginAtZero: true,
+// 				title: {
+// 					display: true,
+// 					text: "Price (in Rupiah)",
+// 				},
+// 			},
+// 		},
+// 	},
+// })
